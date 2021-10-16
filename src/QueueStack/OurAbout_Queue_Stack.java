981513cc2de113922fdedd3d310908a7272ba4ab
MSI <tdv200028@gmail.com>
2021-10-16 16:05:57 +0300

    change2

--- src/QueueStack/OurAbout_Queue_Stack.java
+++ src/QueueStack/OurAbout_Queue_Stack.java
@@ -11,12 +11,7 @@ public class OurAbout_Queue_Stack {
         while ( !command.equalsIgnoreCase( "exit" ) ) {
             switch ( command.toUpperCase() ) {
                 case "PUSH" -> {
-                    try {
-                        tmp.push( in.nextInt() );
-                    }
-                    catch ( Throwable error ) {
-                        System.out.println( "ERROR" );
-                    }
+
                 }
                 case "POP" -> tmp.pop();
                 case "BACK" -> tmp.back();
