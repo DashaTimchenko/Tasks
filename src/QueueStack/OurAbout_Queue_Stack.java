b1bda481b04c496925e123b26d9d419f65dafc0f
Aleksandr <zholud.aleksandr@gmail.com>
2021-10-16 14:31:48 +0300

    braces inline

--- src/QueueStack/OurAbout_Queue_Stack.java
+++ src/QueueStack/OurAbout_Queue_Stack.java
@@ -2,53 +2,40 @@ package QueueStack;
 
 import java.util.*;
 
-
-public class OurAbout_Queue_Stack
-{
-    public static void main( String[] args )
-    {
+public class OurAbout_Queue_Stack {
+    public static void main( String[] args ) {
         Scanner in = new Scanner( System.in );
         OurCollection tmp = new OurQueue();
-        System.out.println(tmp.getClass());
+        System.out.println( tmp.getClass() );
         String command = in.next();
-        while ( !command.equalsIgnoreCase( "exit" ) )
-        {
-            switch ( command.toUpperCase() )
-            {
-                case "PUSH":
-                {
-                    try
-                    {
+        while ( !command.equalsIgnoreCase( "exit" ) ) {
+            switch ( command.toUpperCase() ) {
+                case "PUSH": {
+                    try {
                         tmp.push( in.nextInt() );
                     }
-                    catch ( Throwable error )
-                    {
+                    catch ( Throwable error ) {
                         System.out.println( "ERROR: Not number!" );
                     }
                     break;
                 }
-                case "POP":
-                {
+                case "POP": {
                     tmp.pop();
                     break;
                 }
-                case "BACK":
-                {
+                case "BACK": {
                     tmp.back();
                     break;
                 }
-                case "SIZE":
-                {
+                case "SIZE": {
                     tmp.size();
                     break;
                 }
-                case "CLEAR":
-                {
+                case "CLEAR": {
                     tmp.clear();
                     break;
                 }
-                default:
-                {
+                default: {
                     System.out.println( "ERROR: Unknown command!" );
                 }
             }
